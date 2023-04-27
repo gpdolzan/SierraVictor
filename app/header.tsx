62badd3ba3e9cf5cb1f107ff8122acd70d066fa2
Gabriel Dolzan <gabrieldolzan@icloud.com>
2023-04-27 06:23:02 -0300

    First commit

---
+++ app/header.tsx
@@ -0,0 +1,24 @@
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
+import Link from 'next/link'
+import svgLogo from '../public/svgLogo.svg'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function DolzanHeader() {
+  return (
+    <div className={"mx-auto flex items-center border-b-2 px-6 py-2 h-24"}>
+      <div className="grow">
+        <div className="flex items-center justify-center gap-5">
+          <div>
+            <Link href="/">
+              <h1 className="font-bold text-2xl">Reprogramação</h1>
+              <h1 className="font-bold text-2xl text-center">Genética</h1>
+            </Link>
+          </div>
+          <Image priority src={svgLogo} height={64} width={64} alt="Logo"/>
+        </div>
+      </div>
+    </div>
+  )
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
+import DolzanHeader from './header'
 
 const inter = Inter({ subsets: ['latin'] })
 
